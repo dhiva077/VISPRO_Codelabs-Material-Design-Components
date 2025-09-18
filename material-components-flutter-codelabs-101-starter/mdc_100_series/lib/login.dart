f2cff36104abfb3560b83c4cbde2f46123a2b0d7
Dhiva Kesuma Pertiwi <dhivakesuma@gmail.com>
2025-09-18 12:36:42 +0800

    upload MDC-101

---
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -0,0 +1,141 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+  
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE0D6D1),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                  SizedBox(
+                    width: 150, // ukuran baru, sedikit lebih besar
+                    height: 150,
+                    child: Image.asset('assets/logo.png', fit: BoxFit.contain),
+                  ),
+                  const SizedBox(height: 16.0),
+                  const Text(''),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            // TODO: Remove filled: true values (103)
+
+            // TODO: Add TextField widgets (101)
+            // TODO: Add TextField widgets (101)
+              // [Name]
+              TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFb2a499), // warna background pakai hex
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                  color: Colors.black,           // warna label jadi hitam pekat
+                  fontWeight: FontWeight.bold,   // opsional supaya lebih tegas
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 12.0),
+
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFb2a499),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.black,           // warna label jadi hitam pekat
+                  fontWeight: FontWeight.bold,   // opsional
+                ),
+              ),
+              obscureText: true,
+            ),
+            
+              // TODO: Add button bar (101)
+              const OverflowBar(
+                alignment: MainAxisAlignment.end,
+                // TODO: Add a beveled rectangular border to CANCEL (103)
+                children: <Widget>[
+                  // TODO: Add buttons (101)
+                ],
+              ),
+
+            // TODO: Add button bar (101)
+            // TODO: Add button bar (101)
+            const OverflowBar(
+              alignment: MainAxisAlignment.end,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+              ],
+            ),
+
+            // Spacer
+            const SizedBox(height: 24.0),
+
+            // Tombol CANCEL dan NEXT berdampingan
+            Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                child: const Text('CANCEL'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black, // warna teks tombol CANCEL
+                ),
+                onPressed: () {
+                  // TODO: Clear the text fields (101)
+                  _usernameController.clear();
+                  _passwordController.clear();
+                },
+              ),
+              const SizedBox(width: 16.0),
+              ElevatedButton(
+                child: const Text('NEXT'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFb2a499), // warna background tombol NEXT
+                  foregroundColor: Colors.black,      // warna teks tombol NEXT jadi hitam
+                ),
+                onPressed: () {
+                  // TODO: Show the next page (101)
+                   Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
